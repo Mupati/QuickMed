865f1f8cf63a801d6bf9ecd28260333ec7e4acc8
Mupati <scientificgh@gmail.com>
2017-10-22 22:19:11 +0000

    Update db config

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-    /* I added the following when configuring the postgresql with laravel and heroku application*/
+    /* I added the following when configuring the postgresql with laravel and heroku application
     
     $url = parse_url(getenv("DATABASE_URL"));
 
@@ -9,7 +9,7 @@
     $password = $url["pass"];
     $database = substr($url["path"], 1);    
 
-    /*End of addition*/
+    End of addition*/
 
 return [
 
@@ -65,7 +65,7 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-/*I replaced the commented one below this one with the immediate one*/
+/*I replaced the commented one below this one with the immediate one
             'pgsql' => array(
             'driver'   => 'pgsql',
             'host'     => $host,
@@ -77,7 +77,7 @@ return [
             'schema'   => 'public',
            
             ),
-/*
+*/
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -91,7 +91,7 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-*/
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
