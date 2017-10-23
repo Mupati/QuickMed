c6cfac71d29fd6fc3278f2189901d71b6a5034ab
Mupati <scientificgh@gmail.com>
2017-10-23 22:27:20 +0000

    Modified .env & .gitignore

--- public/n-js/custom.js
+++ public/n-js/custom.js
@@ -21,4 +21,8 @@ $(document).scroll(function(){
 });
 
 
+
+
+
+
 });
--- resources/views/QuickMed/dashboard/dashboard.blade.php
+++ resources/views/QuickMed/dashboard/dashboard.blade.php
@@ -62,5 +62,19 @@
         });
     });
 
+    // ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 350) {        // If page is scrolled more than 50px
+        $('#up').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#up').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#up').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 1000);
+});
+
 </script>
 @endsection
